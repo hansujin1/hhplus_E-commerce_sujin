5365cf97b84a61f9ea915d9fcba686f753dff91f
SUJIN\sujin <sujin.han@gmail.com>
2025-10-31 00:27:05 +0900

    Initial commit

---
+++ src/main/java/com/commerce/hhplus_e_commerce/HhplusECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.hhplus_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusECommerceApplication.class, args);
+    }
+
+}
