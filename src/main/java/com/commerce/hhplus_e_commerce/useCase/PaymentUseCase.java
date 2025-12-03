21816d1c1d81c048a0f3c13e36e4a0d1eabd515a
SUJIN\sujin <sujin.han@gmail.com>
2025-12-03 23:54:19 +0900

    step11/12_refactoring Retryable annotation 추가

--- src/main/java/com/commerce/hhplus_e_commerce/useCase/PaymentUseCase.java
+++ src/main/java/com/commerce/hhplus_e_commerce/useCase/PaymentUseCase.java
@@ -5,6 +5,9 @@ import com.commerce.hhplus_e_commerce.dto.PaymentRequest;
 import com.commerce.hhplus_e_commerce.dto.PaymentResponse;
 import com.commerce.hhplus_e_commerce.facade.PaymentFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,6 +20,7 @@ public class PaymentUseCase {
     
     private final PaymentFacade paymentFacade;
 
+    @Retryable(maxAttempts = 3,backoff = @Backoff(delay = 100, multiplier = 2),value = ObjectOptimisticLockingFailureException.class)
     public PaymentResponse payOrder(Long orderId, PaymentRequest req) {
         try {
             // 포인트 결제 (User 엔티티의 @Version으로 낙관적 락 적용)
